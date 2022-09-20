fa63692ddb6be988ebd2dd00277826ebb5a51e56
Ina Youn <inayoun@microsoft.com>
2022-09-20 17:30:37 -0400

    adding secret

--- src/Back-End/secret-constants.ts
+++ src/Back-End/secret-constants.ts
@@ -1,1 +1,2 @@
+//API KEY for yelp API for restaurant list. Included in .gitignore
 export const API_KEY = "ytcMTZfWs_n0y8uW95qQQQcQ1wJJuJCDmnsyO4wXd5igHiXhx8_-WeNeb6Q7Ir9VPn7M9Jhkm3wrl2x-eRr0AHAws8AI6KMtXKxU0gBjbnHVQuE_EfwZgvh5RiQqY3Yx"
