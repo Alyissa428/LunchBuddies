752650a77d40de2aa5691640a7e37493272b96b1
Eli Newman <elinewman@microsoft.com>
2022-09-21 11:31:08 -0400

    Fixing stuff

--- src/Back-End/user.ts
+++ src/Back-End/user.ts
@@ -3,8 +3,11 @@ import { Question } from './question';
 export interface User {
     name: string;
     alias: string;
+    hometown: string;
+    college: string;
     location: string;
     team: string;
+    language: string;
     age: number;
     questionAnswerPairs: { [key: number]: string[]; };
     userMatches: { [key: string]: number; };
