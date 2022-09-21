5feac9365d4635408289b7a4b36ba9db11cff388
Chris Jenkins <chrisjenkins@microsoft.com>
2022-09-21 16:36:42 -0400

    changed variable names in score.ts to be more intuitive and agreeable with proximity scoring

--- src/Front-End/bot-sso/bot/Back-End/score.ts
+++ src/Front-End/bot-sso/bot/Back-End/score.ts
@@ -9,45 +9,45 @@ export class Score {
         let sumScore = 0.0;
         let numCommonQuestions = 0;
         // iterate through every question of the user with fewer answered questions
-        let qaPairs1 = userA.getQuestionAnswerPairs();
-        let qaPairs2 = userB.getQuestionAnswerPairs();
-        if (qaPairs2.size < qaPairs1.size) {
-            qaPairs1 = userB.getQuestionAnswerPairs();
-            qaPairs2 = userA.getQuestionAnswerPairs();
+        let qaPairsShort = userA.getQuestionAnswerPairs();
+        let qaPairsLong = userB.getQuestionAnswerPairs();
+        if (qaPairsLong.size < qaPairsShort.size) {
+            qaPairsShort = userB.getQuestionAnswerPairs();
+            qaPairsLong = userA.getQuestionAnswerPairs();
             if (debug) console.log("[score.ts] user B has fewer answered questions");
         }
 
-        if (debug) console.log("[score.ts] qaPairs1.size="+qaPairs1.size+", qaPairs2.size="+qaPairs2.size);
+        if (debug) console.log("[score.ts] qaPairsShort.size="+qaPairsShort.size+", qaPairsLong.size="+qaPairsLong.size);
             
-        qaPairs1.forEach((answerList, questionId) => {
+        qaPairsShort.forEach((answersShort, questionId) => {
             let question = questionController.getQuestion(questionId);
             if (debug) console.log("[score.ts] checking for common answers for question `"+questionId+"`... `"+question?.getQuestionText()+"`");
 
             if (question != null){ // Make sure the question ID returned a valid question
-                var answers2 = qaPairs2.get(questionId);
+                var answersLong = qaPairsLong.get(questionId);
                 // Only proceed if second user has also answered this question
-                if (answers2 && answers2.length){
+                if (answersLong && answersLong.length){
                     numCommonQuestions++; //Increment common question counter for average calculation later
                     
-                    let numAnswerMatches = 0;
+                    let thisQuestionScore = 0;
                     //Loop over all of user 1's answers
-                    answerList.forEach(answer1 => {
-                        if (debug) console.log("[score.ts] Checking for `"+answer1+"` in answerList `["+answers2+"]`");
+                    answersShort.forEach(answer1 => {
+                        if (debug) console.log("[score.ts] Checking for `"+answer1+"` in answersShort `["+answersLong+"]`");
                         //If user 2 also has answered this question, count number of matched answers and divide by lower total 
                         //number of selections by either user (in case of "choose up to #" questions where users selected different number of answers)
-                        if (answers2.includes(answer1)) {
-                            numAnswerMatches++;
+                        if (answersLong.includes(answer1)) {
+                            thisQuestionScore += 1;
                             if (debug) console.log("[score.ts] users A and B both have answer `"+answer1+"` for question `"+questionId+"`: +1")
                         } else {
-                            //TODO: implement proximity score for 
+                            //TODO: implement proximity score that adds some amount < 1 for similar answers that dont match
                             // let proximityAnswers = question.answers[answerA];
                             // tempScore = proximityAnswers.has(answerB) ? proximityAnswers[answerB] == null ? 0 : proximityAnswers[answerB] : 0;
                         }
                     });
-                    if (debug) console.log("[score.ts] Compatibility based on question id `"+questionId+"`:",numAnswerMatches / question.getNumberOfAnswersAllowed(),"(answers allowed: "+question.getNumberOfAnswersAllowed()+")");
-                    let thisScore = numAnswerMatches / answerList.length;
-                    if (answers2.length <= answerList.length) {
-                        thisScore = numAnswerMatches / answers2.length;
+                    if (debug) console.log("[score.ts] Compatibility based on question id `"+questionId+"`:",thisQuestionScore / question.getNumberOfAnswersAllowed(),"(answers allowed: "+question.getNumberOfAnswersAllowed()+")");
+                    let thisScore = thisQuestionScore / answersShort.length;
+                    if (answersLong.length <= answersShort.length) {
+                        thisScore = thisQuestionScore / answersLong.length;
                     }
                     sumScore += thisScore;
                 }
