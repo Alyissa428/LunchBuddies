0dacc4a8165f4e6629b51cff17667101442a5393
Alyssa Vu <49544763+alyssa1303@users.noreply.github.com>
2022-09-22 12:41:11 -0400

    Add Cards for Questions 4, 5, 6 (#7)

--- src/Front-End/bot-sso/bot/commands/index.ts
+++ src/Front-End/bot-sso/bot/commands/index.ts
@@ -4,6 +4,9 @@ import { NewUserCommand } from "./newUser";
 import { LearnCommand } from "./learn";
 import { MatchCommand } from "./matchUser";
 import {QuestionnaireConfirmedCommand} from "./questionnaireConfirmed";
+import { OriginCommand } from "./origin";
+import { LanguageCommand } from "./language";
+import { AgeCommand } from "./age";
 import { WeekdayCommand } from "./weekday";
 import { OfficeLocationCommand } from "./officeLocation";
 import { SchoolCommand } from "./school";
@@ -14,6 +17,9 @@ export const commands: BotCommand[] = [
   new LearnCommand(),
   new MatchCommand(),
   new QuestionnaireConfirmedCommand(),
+  new OriginCommand(),
+  new LanguageCommand(),
+  new AgeCommand(),
   new WeekdayCommand(),
   new OfficeLocationCommand(),
   new SchoolCommand()
